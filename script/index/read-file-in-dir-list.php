e39592f7a99f0bf7cc6aa61a54990b215163d6de
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2023-08-23 22:32:24 +0200

    Update read-file-in-dir-list.php

--- script/index/read-file-in-dir-list.php
+++ script/index/read-file-in-dir-list.php
@@ -9,25 +9,12 @@ $blacklist = array("index2.php", "two.txt", "four.html");
 $files = array_diff(glob("**"), $blacklist);
 $fichier ="";
 //$droit = array();
-
 foreach($files as $file){
 $me=posix_getpwuid(posix_getuid($file));
-$me=$me['name'].$file;
+$me=$me['name'];
+$droit =  substr(decoct(fileperms($file)), -4); // 0644
 //var_dump( $me);
-$startscript= $file;
-$fileowneruid=fileowner($startscript);
-
-$fileownerarray=posix_getpwuid($fileowneruid);
-
-$fileowner=array($fileownerarray['name']);
-foreach($fileowner  as $fileownerf){
-
-
-echo "Owner is $fileownerf";
-}
- 
-
-$fichier .= "<div class='post'><a href='" . $_SERVER['PHP_SELF'] . "?file=" . $file . "'><p>" . $file . " === " . $me . "</p></a></div>";		
+$fichier .= "<div class='post'><a href='" . $_SERVER['PHP_SELF'] . "?file=" . $file . "'><p>" . $file . " === " . $me . " === " . $droit . "</p></a></div>";		
 }
 $userinfo = posix_getpwuid(posix_geteuid("lindex.php"));
 echo "This script runs with " . $userinfo["name"] . "'s privileges.";
@@ -40,31 +27,10 @@ $filename = 'index.php';
 echo"<hr>";
 echo substr(decoct(fileperms("read.php")), -4); // 0644
 echo "<hr>";
-$allfilepermcmd = 'sh allfileperm.sh';
-$allfileperm =        shell_exec('sh allfileperm.sh 2>&1'); 
-echo "<pre>"; 
-var_dump( $allfileperm);
-echo "</pre>" ;
 
-echo "<pre>";
-passthru($allfilepermcmd);
-echo "</pre>" ;
-
-while (@ ob_end_flush()); // end all output buffers if any
-
-$proc = popen($allfilepermcmd, 'r');
-echo '<pre>';
-while (!feof($proc))
-{
-    echo fread($proc, 4096);
-    @ flush();
-}
-echo '</pre>';
 
 ?>
 
 <textarea rows="40" cols="100" placeholder="Source code of file" class="source"><?php if(!empty($thesource))echo $thesource; ?></textarea>
 
 </body></html>
-
-
